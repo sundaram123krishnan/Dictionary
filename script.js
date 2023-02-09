73de9183972a8d639091fcc5b916d224e0adbfe2
sundaram123krishnan <krishnsundaram@gmail.com>
2023-02-09 19:18:34 +0530

    created meaning block display

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let word = document.getElementById("word");
 let search = document.getElementById("search");
 let main = document.getElementById("main");
 let url = "";
-
+let meaning = document.createElement("div");
 search.onclick = () => {
   if (word.value != "") {
     url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
@@ -12,12 +12,18 @@ search.onclick = () => {
     p.then((value) => {
       return value.json();
     }).then((value1) => {
-      console.log(value1);
-      console.log(value1[0].meanings[0].definitions[0].definition);
-      let meaning = document.createElement("textarea");
-      word.value = value1[0].meanings[0].definitions[0].definition;
-      meaning.innerHTML = word.value;
+      // console.log(value1);
+      // console.log(value1[0].meanings[0].definitions[0].definition);
+      let val = value1[0].meanings[0].definitions[0].definition;
+      meaning.innerHTML = val;
+      meaning.style.border = "0.25rem solid black";
+      meaning.style.padding = "1rem";
+      meaning.style.width = "auto";
+      meaning.style.fontSize = "1.5rem";
+      meaning.style.fontWeight = "bold";
+      main.insertBefore(meaning,main.children[2])
     });
   }
   word.value = "";
+  main.removeChild(meaning)
 };
